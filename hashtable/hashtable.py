40b8250598ef13d499cdc408036b93156ab57139
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-07-24 22:14:40 +0100

    finished mvp day 1

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,6 +2,7 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -21,8 +22,8 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        # Your code here
-
+        self.capacity = capacity
+        self.array = [None]*capacity
 
     def get_num_slots(self):
         """
@@ -34,8 +35,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        return self.capacity
 
     def get_load_factor(self):
         """
@@ -44,7 +44,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
     def fnv1(self, key):
         """
@@ -55,22 +54,24 @@ class HashTable:
 
         # Your code here
 
-
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
 
         Implement this, and/or FNV-1.
         """
-        # Your code here
-
+        print(key)
+        hash = 5381
+        for x in key:
+            hash = ((hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -81,8 +82,8 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        new_hash = HashTableEntry(key, value)
+        self.array[self.hash_index(key)] = new_hash
 
     def delete(self, key):
         """
@@ -92,8 +93,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        self.array[self.hash_index(key)] = None
 
     def get(self, key):
         """
@@ -103,8 +103,10 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        requested_hash = self.array[self.hash_index(key)]
+        if requested_hash != None:
+            return requested_hash.value
+        return None
 
     def resize(self, new_capacity):
         """
@@ -114,7 +116,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
 
 if __name__ == "__main__":
